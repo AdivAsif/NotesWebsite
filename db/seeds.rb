1e66229a49f59f9f831ac0eff653e1fe97d56cde
AdivAsif <asifadiv@gmail.com>
2020-11-27 02:31:26 -0800

    Added welcome seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+welcome = Note.where(title: 'Welcome').first
+welcome.delete if welcome
+
+notes = Note.create([title: 'Welcome', description: ' to the notes app.'])
