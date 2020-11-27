106ebd56f428ce4c1676998040e7da72771df5d9
AdivAsif <asifadiv@gmail.com>
2020-11-27 10:32:12 -0800

    Added user tests

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -5,7 +5,7 @@ class NotesController < ApplicationController
   # GET /notes
   # GET /notes.json
   def index
-    @notes = Note.all
+    @notes = Note.user_notes(current_user)
   end
 
   # GET /notes/1
@@ -26,6 +26,7 @@ class NotesController < ApplicationController
   # POST /notes.json
   def create
     @note = Note.new(note_params)
+    @note.user = current_user
 
     respond_to do |format|
       if @note.save
